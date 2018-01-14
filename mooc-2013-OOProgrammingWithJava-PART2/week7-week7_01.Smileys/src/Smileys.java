6788aeeeffd1c0402c5fe47abb317ada79e1ec3b
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-14 09:23:46 -0500

    Completed Exercise 1.

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,32 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        printWithSmileys("Method");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
+        printWithSmileys("\\:D/");
+        printWithSmileys("87.");
+        printWithSmileys("Mikael");
+        printWithSmileys("Arto");
     }
 
+    private static void printWithSmileys(String characterString) {
+        String whitespace = "";
+        if (characterString.length() % 2 == 1) {
+            whitespace = " ";
+        }
+        printEncapsulate(characterString.length());
+        System.out.println(":) " + characterString + whitespace + " :)");
+        printEncapsulate(characterString.length());
+    }
+
+    private static void printEncapsulate(int length) {
+        if (length % 2 == 1) {
+            length++;
+        }
+        for (int i = 0; i <= length+4; i=i+2) {
+            System.out.print(":)");
+        }
+        System.out.println("");
+    }
 }
