ad0f5b6e8e09490bb7c6ff1958da60dd5ba993fe
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-15 09:58:11 -0500

    Completed Exercise 5.

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
@@ -6,6 +6,11 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
+        HashMap<String, String> people = new HashMap<String, String>();
+        people.put("matti", "mage");
+        people.put("mikael", "mixu");
+        people.put("arto", "arppa");
+        System.out.println(people.get("mikael"));
     }
 
 }
