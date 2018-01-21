567a043a0ab5cc2b8b31c06d03f6d665c473c1ec
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-21 08:32:40 -0500

    Completed exercise 14.

--- mooc-2013-OOProgrammingWithJava-PART2/week8-week8_14.StudentsSortedByName/src/Student.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week8-week8_14.StudentsSortedByName/src/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class Student implements Comparable<Student> {
 
     private String name;
 
@@ -13,6 +13,17 @@ public class Student {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.name.compareToIgnoreCase(o.getName()) == 0) {
+            return 0;
+        } else if (this.name.compareToIgnoreCase(o.getName()) > 0) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
 }
