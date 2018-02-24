5dd1c31f5730222be00cceefeed148efc48d7bd2
Ryan Snead <ryan.w.snead@gmail.com>
2018-02-24 09:02:23 -0500

    Completed Exercise 29.

--- mooc-2013-OOProgrammingWithJava-PART2/week10-week10_29.PersonAndTheirHeirs/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -1,8 +1,23 @@
+import people.Person;
+import people.Teacher;
+import people.Student;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Main {
-
+    public static void printDepartment(List<Person> people) {
+       // we print all the people in the department
+       for(Person person : people) {
+           System.out.println(person);
+       }
+    }
     public static void main(String[] args) {
         // test your code here
+        List<Person> people = new ArrayList<Person>();
+        people.add( new Teacher("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa", 1200) );
+        people.add( new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki") );
+
+        printDepartment(people);
     }
 }
